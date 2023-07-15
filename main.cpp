5eecf3e1fbb2e991382b598e7699615476772320
Brian01907 <138889888+Brian01907@users.noreply.github.com>
2023-07-15 17:20:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
-#include "mbed.h"
+#include <mbed.h>
 
-// main() runs in its own thread in the OS
-int main()
-{
-    while (true) {
+// This error message means that the compiler cannot find a suitable version of the "printf" function to use.
 
+DigitalIn button(PA_0);
+volatile bool flag = false;
+
+void button_isr() {
+    flag = true;
+}
+
+int main() {
+    button.rise(&button_isr);
+
+    while (1) {
+        if (flag) {
+            printf("Button pressed\n");
+            flag = false;
+        }
     }
 }
+
 
